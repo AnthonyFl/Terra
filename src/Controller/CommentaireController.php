11667761f2e2f322482dd80ce62d3901e804902a
AnthonyFl <38648446+AnthonyFl@users.noreply.github.com>
2018-06-19 15:58:06 +0200

    Form ok (en attente du front)

--- src/Controller/CommentaireController.php
+++ src/Controller/CommentaireController.php
@@ -4,13 +4,36 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use App\Entity\Poi;
-use App\Entity\Membre;
+use App\Entity\Commentaire;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\CommentaireType;
 
 
 class CommentaireController extends Controller
 {
+/**
+     * @Route("/comment/create", name="createComment")
+     */
+    public function createCommentAction(Request $request){
+        $comment = new Commentaire;
+        $form = $this -> createForm(CommentaireType::class, $comment);
 
+        if($request -> isMethod('POST') && $form -> handleRequest($request) -> isValid()){
+            
+            $em = $this -> getDoctrine() -> getManager();
+            $em -> persist($comment);
+            $em -> flush();
+
+            $request -> getSession() -> getFlashBag() -> add('success', 'Félicitations, vous avez ajouter un commentaire');
+            return $this -> redirectToRoute('createComment');
+        }
+
+        $formView = $form -> createView();
+        $params = array(
+            'commentForm' => $formView,
+            'title' => "Ajout de commentaire"
+        );
+
+        return $this->render('commentcreate.html.twig', $params);
     }
+}
